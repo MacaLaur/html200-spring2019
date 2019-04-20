0ef35fb54af2c478c129607eb8c330bace359332
macall <lauren.macalalad@gmail.com>
2019-04-20 11:26:02 -0700

    create arrays and manipulate them

---
+++ lesson03/exercise-one.js
@@ -0,0 +1,8 @@
+let names = ['Ricky', 'Lauren', 'Molly', 'Sidney', 'Lindsey', 'Kelly', 'Kristel'];
+console.log(names);
+names.push('Willis');
+console.log(names);
+names.splice(2, 1);
+console.log(names);
+let str = names.join(', ');
+console.log(str);
